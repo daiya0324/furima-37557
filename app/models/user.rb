709c04e69e171dab2cfd6a0cfdd7346b27b5c50a
daiya0324 <daiya19980324@gmail.com>
2022-02-23 18:23:26 +0900

    ユーザーモデルテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
     validates :last_name_kana
   end
   validates :birthday, presence: true
-  
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
 end
