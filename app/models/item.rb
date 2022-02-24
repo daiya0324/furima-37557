0923142b47ba3cc2052c55f97472a7293b59a472
daiya0324 <daiya19980324@gmail.com>
2022-02-24 12:36:24 +0900

    バリデーション、ルーティング設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :shipping_fee_burden_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_date_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 end
