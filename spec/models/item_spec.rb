6a60b891c4106e11b9b9fe6bc54163b6aef51f01
daiya0324 <daiya19980324@gmail.com>
2022-02-25 20:32:44 +0900

    テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -71,10 +71,45 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price must be greater than 299'
       end
-      it 'priceが¥9999999より少ない時は出品できない' do
+      it 'priceが¥9999999より大きい時は出品できない' do
         @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price must be less than 10000000'
+      end
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+      end
+      it '商品の状態に「---」が選択されている場合は出品できない' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Status can't be blank"
+      end
+      it '配送料の負担に「---」が選択されている場合は出品できない' do
+        @item.shipping_fee_burden_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping fee burden can't be blank"
+      end
+      it '発送元の地域に「---」が選択されている場合は出品できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Prefecture can't be blank"
+      end
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
+        @item.shipping_date_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping date can't be blank"
+      end
+      it '価格に半角数字以外が含まれている場合は出品できない' do
+        @item.price = 'aaaaa'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+      it 'userが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "User must exist"
       end
     end
   end
