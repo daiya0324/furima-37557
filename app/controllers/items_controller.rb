6b1f1131f9ac88ec3fe96ba1329f0c183280a2af
daiya0324 <daiya19980324@gmail.com>
2022-02-28 20:28:55 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destory]
   before_action :set_tweet, only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index, only: [:destroy, :edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -23,7 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to action: :index unless current_user.id == @item.user_id
   end
 
   def update
@@ -48,5 +48,9 @@ class ItemsController < ApplicationController
 
   def set_tweet
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
